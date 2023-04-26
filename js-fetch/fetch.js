c7a4f14cd69f931d09ac0e2a06363936bb1574f4
BoboWab99 <wingabire.arnaud@strathmore.edu>
2023-04-26 20:46:59 +0300

    fetch example with styles & python @todo

--- js-fetch/fetch.js
+++ js-fetch/fetch.js
@@ -1,4 +1,62 @@
 
+const resultContainer = document.getElementById('result')
+
+_fetch('https://cat-fact.herokuapp.com/facts', displayResult)
+
+
+/**
+ * @param {Array} data array of facts
+ */
+function displayResult(data) {
+
+   const list = document.createElement('ul')
+   list.classList.add('fact-list')
+
+   data.forEach(fact => {
+      // console.log(fact.text)
+      // console.log(fact.createdAt.slice(0, 10))
+
+      const dateCreated = fact.createdAt.slice(0, 10)
+      const userFirstLetter = fact.user.slice(0, 1)
+
+      const liText = `
+      <li class="fact-list-item">
+         <div class="fact-user-profile">${userFirstLetter}</div>
+         <div class="fact-content">
+            ${fact.text}
+            <div class="fact-metadata">
+               <span class="fact-user">${fact.user}</span>
+               <span class="fact-created-at">${dateCreated}</span>
+            </div>
+         </div>
+      </li>`
+
+      // const listItem = document.createElement('li')
+      // listItem.innerHTML = fact.text
+      // listItem.classList.add('fact-list-item')
+      const listItem = htmlStringToElement(liText)
+      list.appendChild(listItem)
+   })
+
+   resultContainer.appendChild(list)
+}
+
+
+/**
+ * @param {String} html single element html string
+ * @returns {Element}
+ */
+function htmlStringToElement(html) {
+   const t = document.createElement('template')
+   t.innerHTML = html.trim()
+   return t.content.firstElementChild
+}
+
+
+// these two functions '_fetch' and '_fetch2' do the same thing
+// just two different styles of writing promises!
+
+
 /**
  * Fetch API helper function
  * @param {String} url target url
@@ -38,6 +96,7 @@ async function _fetch(url, callback, method = 'GET', data) {
       console.error('Error:', error);
    });
 }
+
 
 /**
  * Fetch API helper function
